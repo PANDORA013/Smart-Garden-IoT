c6eda2aaf9a354cac8d0582a28c268c1633feb85
Thomas Aruan <thomas.aruan22@student.uisi.ac.id>
2026-01-08 00:51:03 +0700

    feat: Add dedicated SPA route and view for minimalist React dashboard

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('universal-dashboard');
 });
+
+// Route untuk React SPA (Minimalist Design)
+Route::get('/spa-dashboard', function () {
+    return view('spa-dashboard');
+});
