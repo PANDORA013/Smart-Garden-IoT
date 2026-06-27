93673d94f3d6caff3b0dac971e8cbd64dc87b494
Thomas Aruan <thomas.aruan22@student.uisi.ac.id>
2026-06-27 17:23:48 +0700

    Simplify api/index.php and routes based on best practices

--- api/index.php
+++ api/index.php
@@ -1,22 +1,3 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
-// Create database directory and file if they don't exist
-$dbPath = '/tmp/database.sqlite';
-if (!file_exists($dbPath)) {
-    touch($dbPath);
-}
-
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-// Run migrations if database is empty
-try {
-    $app->make('Illuminate\Contracts\Console\Kernel')->call('migrate', ['--force' => true]);
-} catch (Exception $e) {
-    // Ignore migration errors
-}
-
-$app->make(Illuminate\Contracts\Http\Kernel::class)
-    ->handle(Illuminate\Http\Request::capture())
-    ->send();
+require __DIR__ . '/../public/index.php';
